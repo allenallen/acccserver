b9ec9c092a1563025b7a3b96bf01704eafc669d3
Allen Arcenal <eaarcenal@inovotek.com>
2017-03-13 14:21:57 +0800

    router update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var app = express()
 var admin = require("firebase-admin");
+var url = require( "url" );
+var queryString = require( "querystring" );
 var serviceAccount = require("../accckey.json");
+var bodyParser = require("body-parser");
+var url = require( "url" );
+var queryString = require( "querystring" );
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -13,49 +25,154 @@ admin.initializeApp({
 
 var db = admin.database();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-      var ref = db.ref("Parts");
-      ref.set({
-                    parts1: {
-                      date: "June 23, 1912",
-                      name: "screw"
-                    },
-                    parts2: {
-                      date: "December 9, 1906",
-                      name: "nail"
-                    }
+router.get('/query_customer', function(req,res,next){
+      console.log("here");
+      var theUrl = url.parse( req.url );
+      console.log(theUrl);
+      var queryObj = queryString.parse( theUrl.query );
+      console.log(queryObj);
+      var obj = JSON.parse( queryObj );
+
+      var ref = db.ref("Customer");
+      console.log("here2");
+      console.log(obj);
+
+      switch(obj.query){
+            case "single":
+                  ref.orderByChild("lastname").startAt(obj.lastname).on("value", function(snapshot){
+                        res.json(snapshot);
                   });
+                  break;
+            default:
+                  var ref = db.ref("Customer");
+                  ref.orderByChild("lastname").on("value", function(snapshot){
+                        res.json(snapshot);
+                  });
+      }
 
-ref.on("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-  res.json({"home": "welcome"});
-});
 
-router.get('/about', function(req,res){
-	res.render('about', {title: 'My Bags bite'});
 });
 
-router.get('/login', function(req,res){
-	res.render('login',{title: 'My Bags bite'});
-});
+router.post('/new_customer', function(req,res,next){
+      var ref = db.ref("Customer");
+      var data = {};
+      data["firstname"] =  req.body.firstname;
+      data["lastname"] =  req.body.lastname;
+      data["address"] =  req.body.address;
+      data["carmake"] =  req.body.carmake;
+      data["plate"] =  req.body.plate;
+      data["contact"] =  req.body.contact;
 
-router.get('/register',function(req,res){
-	res.render('register');
-});
+      var saveRef = ref.push();
 
-router.get('/logout', function(req,res){
-      Firebase.auth().signOut().then(function(){
-            req.session.destroy();
-            res.redirect('/');
-      }, function(error){
-            console.log(error);
+      saveRef.set(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  data["id"] = saveRef.key;
+                  res.json({"status":"data saved", data:data});
+            }
       });
-
 });
+
+router.post('/update_customer', function (req,res,next){
+      var child = db.ref("Customer").child(req.body.id);
+      var data = {};
+      data["firstname"] =  req.body.firstname;
+      data["lastname"] =  req.body.lastname;
+      data["address"] =  req.body.address;
+      data["carmake"] =  req.body.carmake;
+      data["plate"] =  req.body.plate;
+      data["contact"] =  req.body.contact;
+
+      child.update(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  res.json({status:"data saved",data:data});
+            }
+      });
+});
+
+router.post('/new_employee', function(req,res,next){
+      var ref = db.ref("Employee");
+      var data = {};
+      data["firstname"] =  req.body.firstname;
+      data["lastname"] =  req.body.lastname;
+      data["address"] =  req.body.address;
+      data["contact"] =  req.body.contact;
+
+      var saveRef = ref.push();
+
+      saveRef.set(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  data["id"] = saveRef.key;
+                  res.json({"status":"data saved",data:data});
+            }
+      });
+});
+
+router.post('/update_employee', function (req,res,next){
+      var child = db.ref("Employee").child(req.body.id);
+      var data = {};
+      data["firstname"] =  req.body.firstname;
+      data["lastname"] =  req.body.lastname;
+      data["address"] =  req.body.address;
+      data["contact"] =  req.body.contact;
+
+      child.update(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  res.json({"status":"data saved",data:data});
+            }
+      });
+});
+
+router.post('/new_job_order', function(req,res,next){
+      var ref = db.ref("Job Order/"+req.body.jonumber);
+      var data = {};
+      data["customerid"] =  req.body.customerid;
+      data["date"] =  req.body.date;
+      data["employees"] =  req.body.employees;
+      data["jobdescription"] =  req.body.jobdescription;
+      data["partsids"] =  req.body.partsids;
+      data["labor"] =  req.body.labor;
+      data["totalamount"] =  req.body.totalamount;
+
+      ref.set(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  data["jonumber"] = req.body.jonumber;
+                  res.json({"status":"data saved",data:data});
+            }
+      });
+});
+
+router.post('/update_job_order', function (req,res,next){
+      var child = db.ref("Job Order").child(req.body.jonumber);
+      var data = {};
+      data["customerid"] =  req.body.customerid;
+      data["date"] =  req.body.date;
+      data["employees"] =  req.body.employees;
+      data["jobdescription"] =  req.body.jobdescription;
+      data["partsids"] =  req.body.partsids;
+      data["labor"] =  req.body.labor;
+      data["totalamount"] =  req.body.totalamount;
+
+      child.update(data, function(error){
+            if(error){
+                  res.json({"error":error});
+            } else {
+                  res.json({"status":"data saved",data:data});
+            }
+      });
+});
+
+
 
 
 module.exports = router;
